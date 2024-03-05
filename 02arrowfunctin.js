dcee879999b50c6861abcc7978d40dfc3ee04ec9
Sarthak Patel <sarthakpatel231@gmail.com>
2024-03-05 13:14:38 +0530

    arrow function implemented

---
+++ 02arrowfunctin.js
@@ -0,0 +1,11 @@
+// arrow function 
+//syntax
+//// Syntax:
+//const functionName = (parameters) => {
+  // function body
+  //return result;
+//};
+const multiply=(a,b)=>{
+  return a*b;
+}
+console.log(multiply(3,5))
