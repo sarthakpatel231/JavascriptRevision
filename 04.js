283bae34c0dcacefa18746b0c9ed823cce9c7d3c
Sarthak Patel <sarthakpatel231@gmail.com>
2024-03-05 14:09:21 +0530

    string spread added

--- 04.js
+++ 04.js
@@ -3,7 +3,7 @@
 //array spread
 const arr1=[2,3,5,6];
 const arr2=[67,78,45];
-const arr3=[...arr1,...arr2,3,4,54];
+const arr3=[...arr1,...arr2,3,4,54];// return a new Array
 console.log(arr3);
 // object spread
 const person={
@@ -12,3 +12,13 @@ const person={
 }
 const fullname={...person,lastNmae:"the prince of gowtham"};
 console.log(fullname);
+// function 
+function sum(...numbers){
+  console.log(numbers);
+}
+console.log(sum(3,5,6,36))
+// string spread
+const str = 'hello';
+const chars = [...str];
+
+console.log(chars); // Output: ['h', 'e', 'l', 'l', 'o']
